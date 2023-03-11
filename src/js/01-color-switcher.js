32f1ecdc6ce595ac4288e6447c829fbc674bb43e
Nikolas82 <nikobogdan34@gmail.com>
2023-03-11 18:47:14 +0200

    task 1

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,28 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const TIME_DELAY = 1000;
+const bodyEl = document.querySelector('body');
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+
+let timerId = null;
+
+startBtn.addEventListener('click', handlerStart);
+
+stopBtn.addEventListener('click', handlerStop);
+
+function handlerStart() {
+  startBtn.disabled = true;
+
+  timerId = setInterval(() => {
+    const color = getRandomHexColor();
+    bodyEl.style.backgroundColor = color;
+  }, TIME_DELAY);
+}
+
+function handlerStop() {
+  clearInterval(timerId);
+
+  startBtn.disabled = false;
+}
